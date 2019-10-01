e12144cf455d4e0da9f5610a6f46d7c3444c9c75
AnthonyCQuiroz <56050507+AnthonyCQuiroz@users.noreply.github.com>
2019-10-01 14:50:42 -0700

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,9 @@
+# Anthony Quiroz 
+number1= input("Input a number:") 
+number2= input("Input a number:")
+number1 = int(number1)
+number2 = int(number2)
+print(str(number1) + "+"+ str(number2) + "="+ str(number1 + number2))
+print(str(number1) + "-"+ str(number2) + "="+ str(number1 - number2))
+print(str(number1) + "*"+ str(number2) + "="+ str(number1 * number2))
+print(str(number1) + "/"+ str(number2) + "="+ str(number1 / number2))
